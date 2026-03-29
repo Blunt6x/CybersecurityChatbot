405aa00e77c976f0313bc1c7e4b6f55f05021da6
Blunt6x <lungilesibanda887@gmail.com>
2026-03-29 14:52:33 +0200

    AudionHelper feature added (still need to record wav greeting).

---
+++ CybersecurityChatbot/AudioHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Media;
+
+namespace CybersecurityChatbot
+{
+    public class AudioHelper
+    {
+      
+       public void Play() 
+        {
+            try
+            {
+
+                SoundPlayer player = new SoundPlayer("path_to_audio_file.wav");
+                player.Play();
+            } catch (Exception ex)
+            { 
+            Console.WriteLine("Message could not be loaded"+ex.Message);
+            }
+
+        }
+    }
+}
